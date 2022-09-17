51df5331c94241119fae4ba0099404f7a0c32eb9
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-09-17 21:58:48 +0700

    Source

---
+++ OsKmitl/experimentThreedotOne.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+ืnamespace Lab_OS_Concurrency01
+{
+class Program
+{
+    static int resource = 10000;
+    static void TestThread1(){
+        int i ; 
+        for(i=0;i<4555;i++){
+            resource++;
+            Console.Write(".";
+        }
+    }
+    static void Main(string[]args){
+        Thread th1 = new Thread(TestThread1);
+        th1.Start();
+        Thread.Sleep(10);
+        Console.WriteLine("resource={0}",resource);
+    }
+}
+}
