817f92e0cf7f71c63da1e14b2b2e0ab7f77f1286
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-10-16 21:29:06 +0700

    Source

---
+++ Synchronization/Ex-01.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace OS_Sync_Ex_01
+{
+    class Program
+    {
+        private static int sum = 0;
+        static void plus()
+        {
+            int i ;
+            for (int i = 0; i < 1000001; i++)
+            {
+                sum += i ;
+            }
+        }
+        static void minus()
+        {
+            int i ; 
+            for(i=0;i<1000000;i++)
+            {
+                sum -= 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread P = new Thread(new ThreadStart(plus));
+            Thread M = new Thread(new ThreadStart(minus));
+
+            Stopwatch sw  = new Stopwatch();
+            Console.WriteLine("Start..");
+            sw.Start();
+
+            P.Start();
+            M.Start();
+
+            P.Join();
+            M.Join();
+
+            sw.Stop();
+            Console.WriteLine("sum = {0}",sum);
+            Console.WriteLine("Time used :}"+sw.ElapsedMilliseconds.ToString()+ "ms");
+        }
+    }
+}
